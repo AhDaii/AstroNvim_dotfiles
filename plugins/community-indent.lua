0e7d94c4f0adaa49c54bc935b216c0c93b8b0371
AhDaii <1171469399@qq.com>
2023-10-11 11:12:02 +0800

    update indent

--- plugins/community-indent.lua
+++ plugins/community-indent.lua
@@ -1,6 +1,17 @@
 return {
-    {import = "astrocommunity.indent.indent-blankline-nvim"},
-    {import = "astrocommunity.indent.mini-indentscope"}, {
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        event = "User AstroFile"
+        -- opts = {
+        -- char = "│",
+        -- filetype_exclude = {
+        --     "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy"
+        -- },
+        -- show_trailing_blankline_indent = false,
+        -- show_current_context = false
+        -- }
+    }, {import = "astrocommunity.indent.mini-indentscope"}, {
         "echasnovski/mini.indentscope",
         opts = {
             symbol = "╎",
